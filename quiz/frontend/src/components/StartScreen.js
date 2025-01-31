6d415872ecd4c1a6603c39a512eb164db0d495b1
EmersonW Silva <emrsnwslv@gmail.com>
2025-01-30 23:26:05 -0300

    Update Identidade visual

--- quiz/frontend/src/components/StartScreen.js
+++ quiz/frontend/src/components/StartScreen.js
@@ -2,18 +2,22 @@ import React from 'react';
 import '../styles/StartScreen.css';
 
 const StartScreen = ({ onStart }) => {
-    return (
-        <div className="start-screen">
-            <div className="start-container">
-                <h1>Codi Quiz</h1>
-                <p>
-                    Bem-vindo ao Codi Quiz! <br />
-                    Teste seus conhecimentos de desenvolvimento Full Stack.
-                </p>
-                <button onClick={onStart}>Iniciar Quiz</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="start-screen">
+      <div className="start-container">
+        <img
+          src="/tecnoQuizLogo.webp"
+          alt="Tecno Quiz Logo"
+          className="logo"
+        />
+        <p>
+          Bem-vindo ao Tecno Quiz! <br />
+          Teste seus conhecimentos de Desenvolvimento Full Stack.
+        </p>
+        <button onClick={onStart}>Iniciar Quiz</button>
+      </div>
+    </div>
+  );
 };
 
 export default StartScreen;
